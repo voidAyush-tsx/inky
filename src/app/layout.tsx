2c663eb28d56f5efdf364688299ef2254aa7b544
Ayush007-pro <ayush82194@gmail.com>
2025-10-23 07:47:24 +0530

    add Twitter image card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,20 +14,29 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Inky - Where Storytelling Comes Alive",
-  description: "Publishing, design, and multimedia solutions for creators want to leave a mark",
+  description: "Building scalable and innovative web solutions.",
   openGraph: {
     title: "Inky - Where Storytelling Comes Alive",
-    description: "Publishing, design, and multimedia solutions for creators want to leave a mark",
-    images: ["/og"],
+    description: "Building scalable and innovative web solutions.",
+    siteName: "Inky",
+    // url: "your site url here",
+    images: [
+      {
+        url: "https://yourdomain.com/og-image.png", //change this to your domain after hosting website
+        width: 1200,
+        height: 630,
+        alt: "Inky - Where Storytelling Comes Alive",
+      },
+    ],
+    locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "Inky - Where Storytelling Comes Alive",
     description: "Publishing, design, and multimedia solutions for creators want to leave a mark",
-    images: ["/og"],
+    images: ["https://yourdomain.com/og-image.png"], //change this to your domain after hosting website
   },
-  themeColor: "#0B1220",
 };
 
 export default function RootLayout({
